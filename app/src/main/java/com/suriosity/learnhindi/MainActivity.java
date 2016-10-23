2650d3103ac212ff5c20ac835d2b464f73d861c8
Macbook <nalinsuri@gmail.com>
2016-10-22 22:31:41 -0400

    add Intents to open activities

--- app/src/main/java/com/suriosity/learnhindi/MainActivity.java
+++ app/src/main/java/com/suriosity/learnhindi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.suriosity.learnhindi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,21 +14,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeView(View v){
+        Intent i;
+
         switch (v.getId()){
             case R.id.numbers_category:
                 System.out.println("openNumbersView Function Called");
+                i = new Intent(this, Numbers.class);
+                startActivity(i);
                 break;
             case R.id.days_category:
                 System.out.println("openDaysView Function Called");
+                i = new Intent(this, Days.class);
+                startActivity(i);
                 break;
             case R.id.relationships_category:
                 System.out.println("openRelationships Function Called");
+                i = new Intent(this, Relationships.class);
+                startActivity(i);
                 break;
             case R.id.fruits_and_vegetables_category:
                 System.out.println("openFruitsandVegetablesView Function Called");
+                i = new Intent(this, FruitsAndVegetables.class);
+                startActivity(i);
                 break;
             case R.id.pronouns_category:
                 System.out.println("openPronounsView Function Called");
+                i = new Intent(this, Pronouns.class);
+                startActivity(i);
                 break;
         }
     }
