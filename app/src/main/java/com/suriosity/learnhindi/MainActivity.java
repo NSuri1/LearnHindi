69228644029c79bac6194f522a4f3cb508292c1c
Macbook <nalinsuri@gmail.com>
2016-10-22 22:06:24 -0400

    add openView functions and onclick

--- app/src/main/java/com/suriosity/learnhindi/MainActivity.java
+++ app/src/main/java/com/suriosity/learnhindi/MainActivity.java
@@ -1,7 +1,8 @@
 package com.suriosity.learnhindi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersView(View v){
+        System.out.println("openNumbersView Function Called");
+        setContentView(R.layout.activity_numbers);
+    }
+
+    public void openDaysView(View v){
+        System.out.println("openDaysView Function Called");
+    }
+
+    public void openRelationshipsView(View v){
+        System.out.println("openRelationships Function Called");
+    }
+
+    public void openFruitsAndVegetablesView(View v){
+        System.out.println("openFruitsandVegetablesView Function Called");
+    }
+
+    public void openPronounsView(View v){
+        System.out.println("openPronounsView Function Called");
     }
 }
