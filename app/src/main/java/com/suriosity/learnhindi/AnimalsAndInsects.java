3fc80b2b61dadb9381a62ef61224d658fad56b9f
Macbook <nalinsuri@gmail.com>
2016-10-23 21:04:34 -0400

    Error on Animals and Insects createViews()

--- app/src/main/java/com/suriosity/learnhindi/AnimalsAndInsects.java
+++ app/src/main/java/com/suriosity/learnhindi/AnimalsAndInsects.java
@@ -3,31 +3,42 @@ package com.suriosity.learnhindi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class AnimalsAndInsects extends AppCompatActivity {
 
+    ArrayList<String> englishWords = new ArrayList<String>(Arrays.asList("meat", "fat", "fish",
+            "chicken", "egg", "cow", "buffalo", "milk", "horns",
+            "tail", "goat", "dog", "snake", "monkey", "mosquito",
+            "ant", "spider"));
+
+    ArrayList<String> hindiTranslation = new ArrayList<String>(Arrays.asList("gosht (गोश्त)", "charbi (चर्बी)", "machhli (मछली)",
+            "murgi (मुर्गी)", "anda (अण्डा)", "gae (गाय)", "bhaens (भैंस)", "dudh (दूध)", "seeng (सींग)",
+            "dum (दुम)", "bakri (बकरी)", "kutta (कुत्ता)", "saanp (साँप)", "bandar (बन्दर)", "machchhar (मच्छर)",
+            "chinti (चींटी)", "makri (मकडी)"));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals_and_insects);
 
-        ArrayList<String> englishWords = new ArrayList<String>(Arrays.asList("meat", "fat", "fish",
-                                         "chicken", "egg", "cow", "buffalo", "milk", "horns",
-                                         "tail", "goat", "dog", "snake", "monkey", "mosquito",
-                                         "ant", "spider"));
+        createViews();
 
-        ArrayList<String> hindiTranslation = new ArrayList<String>(Arrays.asList("gosht (गोश्त)", "charbi (चर्बी)", "machhli (मछली)",
-                                             "murgi (मुर्गी)", "anda (अण्डा)", "gae (गाय)", "bhaens (भैंस)", "dudh (दूध)", "seeng (सींग)",
-                                             "dum (दुम)", "bakri (बकरी)", "kutta (कुत्ता)", "saanp (साँप)", "bandar (बन्दर)", "machchhar (मच्छर)",
-                                             "chinti (चींटी)", "makri (मकडी)"));
+    }
 
 
+    public void createViews(){
         LinearLayout rootView = (LinearLayout) findViewById(R.id.numbers_root_view);
+        TextView wordView = new TextView(this);
 
-        
+        wordView.setText(englishWords.get(0));
+
+        rootView.addView(wordView);
+
+
     }
 
 
